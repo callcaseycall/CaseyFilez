47d54d1c85654072fda9a566dba049cb41de781a
callcaseycall <callcaseycall@gmail.com>
2025-09-16 21:12:33 -0600

    First commit, completed seed 3 folders with 5 files

--- db/seed.js
+++ db/seed.js
@@ -1,4 +1,5 @@
 import db from "#db/client";
+import { faker } from "@faker-js/faker";
 
 await db.connect();
 await seed();
@@ -7,4 +8,24 @@ console.log("🌱 Database seeded.");
 
 async function seed() {
   // TODO
+  const folderNames = ["projects", "media", "documents"];
+
+  for (const name of folderNames) {
+    const {
+      rows: [folder],
+    } = await db.query(`INSERT INTO folders (name) VALUES ($1) RETURNING *`, [
+      name,
+    ]);
+
+    for (let i = 0; i < 5; i++) {
+      await db.query(
+        `INSERT INTO files (name, size, folder_id) VALUES ($1, $2, $3)`,
+        [
+          faker.system.fileName(),
+          faker.number.int({ min: 100, max: 10000 }),
+          folder.id,
+        ]
+      );
+    }
+  }
 }
